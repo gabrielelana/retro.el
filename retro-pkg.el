65442d053902b3bb66f9ddb4ce718ba11a990594
gabriele.lana <gabriele.lana@gmail.com>
2023-09-08 18:34:04 +0200

    Update package file

--- retro-pkg.el
+++ retro-pkg.el
@@ -1,2 +1,4 @@
-(define-package "retro.el" "0.0.1" "Library to create retro games for Emacs in EmacsLisp"
-  '((ht "2.0.0")))
+(define-package "retro" "0.0.1" "Library to create retro games for Emacs in EmacsLisp"
+  '((ht "2.0.0")
+    (emacs "28.1")
+    (dash "2.12.0")))
